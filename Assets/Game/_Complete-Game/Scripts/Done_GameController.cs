73dd4584f1983a9523d3e1d7701f3b74e331e337
matufajus <matas.tas@gmail.com>
2017-11-14 23:42:58 +0200

    added StartMenu scene.

---
+++ Assets/Game/_Complete-Game/Scripts/Done_GameController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Done_GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+    public GUIText livesText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+    public int Lives { get; private set; }
+
+    public AudioSource powerupPickup;
+    public AudioSource soundtrack;
+
+    void Start()
+    {
+        soundtrack.volume = PlayerPrefs.GetFloat("MusicSliderVolumeLevel", soundtrack.volume);
+        powerupPickup.volume = PlayerPrefs.GetFloat("SoundEffSliderVolumeLevel", powerupPickup.volume);
+        Lives = 3;
+        gameOver = false;
+        restart = false;
+        livesText.text = "";
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        UpdateLives();
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                if (hazard.tag == "Hole")
+                    yield return new WaitForSeconds(1);
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    public void LoseLife()
+    {
+        Lives = Lives - 1;
+        if (Lives <= 0) GameOver();
+        UpdateLives();
+    }
+
+    public void LoseAllLifes()
+    {
+        Lives = 0;
+        if (Lives <= 0) GameOver();
+        UpdateLives();
+    }
+
+    public void GainLife()
+    {
+        Lives = Lives + 1;
+        UpdateLives();
+    }
+
+    public void UpdateLives()
+    {
+        livesText.text = "Lives: " + Lives;
+    }
+
+    public void PickupPowerup()
+    {
+        powerupPickup.Play();
+    }
+
+}
---
+++ Assets/StartMenu/AudioSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AudioSettings : MonoBehaviour
+{
+
+    public Slider musicVolume;
+    public Slider soundEffVolume;
+    public AudioSource audioSource;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    audioSource.volume = musicVolume.value;
+	}
+
+    public void SaveSliderValue()
+    {
+        PlayerPrefs.SetFloat("MusicSliderVolumeLevel", musicVolume.value);
+        PlayerPrefs.SetFloat("SoundEffSliderVolumeLevel", soundEffVolume.value);
+    }
+}
